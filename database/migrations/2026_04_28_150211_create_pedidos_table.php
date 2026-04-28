e6b0d22df9b491b14a8366c0413699cdae333cae
Samuel <samuel.lopez.perez2004@gmail.com>
2026-04-28 17:21:16 +0200

    creacion base de datos

---
+++ database/migrations/2026_04_28_150211_create_pedidos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('estado_pedido');
+            $table->decimal('total', 8, 2);
+            $table->string('direccion_envio');
+            $table->date('fecha_pedido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
